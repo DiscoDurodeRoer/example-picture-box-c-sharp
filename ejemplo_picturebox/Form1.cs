587b0760cdde7e3b6f41b4b8d1b707bc957bd4b2
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-09 23:15:48 +0200

    example picture box C#

---
+++ ejemplo_picturebox/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplo_picturebox
+{
+    public partial class Form1 : Form
+    {
+
+        private string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Abrir = new OpenFileDialog();
+            Abrir.Filter = "JPEG(*.JPG)|*.JPG|BMP(*.BMP)|*.BMP";
+            if (Abrir.ShowDialog() == DialogResult.OK)
+            {
+                path = Abrir.FileName;
+                pictureBox1.Image = Image.FromFile(Abrir.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.Copy(path, "images/image.jpg", true);
+        }
+    }
+}
